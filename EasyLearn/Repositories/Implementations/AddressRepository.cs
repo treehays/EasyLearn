d38c4715c84b7a1b8ecbac75b686b6ca3e879532
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2023-03-22 09:16:03 +0100

    recomited all changes

---
+++ EasyLearn/Repositories/Implementations/AddressRepository.cs
@@ -0,0 +1,9 @@
+﻿using EasyLearn.Models.Entities;
+using EasyLearn.Repositories.Interfaces;
+
+namespace EasyLearn.Repositories.Implementations
+{
+    public class AddressRepository : BaseRepository<Address>, IAddressRepository
+    {
+    }
+}
